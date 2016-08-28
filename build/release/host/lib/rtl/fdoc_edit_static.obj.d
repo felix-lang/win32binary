7c42cfb2925170b11c7e6e68cd49c5c8290fdbc3
skaller <skaller@users.sourceforge.net>
2016-08-28 06:23:38 +0000

    Appveyor rebuild

--- build/release/host/lib/rtl/fdoc_edit_static.obj.d
+++ build/release/host/lib/rtl/fdoc_edit_static.obj.d
@@ -3,7 +3,7 @@ c:\users\appveyor\.felix\cache\text\c\projects\felix\build\release\share\lib\plu
 build\release\share\lib\rtl\flx_rtl.hpp
 c:\projects\felix\build\release\share\lib\rtl\flx_rtl_config.hpp
 c:\projects\felix\build\release\share\lib\rtl\flx_rtl_config.h
-build\release\host\lib\rtl\flx_rtl_config_params.hpp
+build\release\win32\lib\rtl\flx_rtl_config_params.hpp
 c:\projects\felix\build\release\share\lib\rtl\flx_exceptions.hpp
 c:\projects\felix\build\release\share\lib\rtl\flx_exceptions_config.hpp
 c:\projects\felix\build\release\share\lib\rtl\flx_gc.hpp
