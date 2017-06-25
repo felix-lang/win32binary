5bf46a56d248273a56b64a5b11fe3b96f871c41a
skaller <skaller@users.sourceforge.net>
2017-06-25 02:36:47 +0000

    Appveyor rebuild

--- build/release/src/compiler/flx_version_hook/flx_version_hook.ml
+++ build/release/src/compiler/flx_version_hook/flx_version_hook.ml
@@ -2,8 +2,8 @@ open Flx_version
 let version_data: version_data_t =
 {
   version_string = "2016.07.12-rc1";
-  build_time_float = 1498214611.1874595;
-  build_time = "Fri Jun 23 10:43:31 2017";
+  build_time_float = 1498356755.8211539;
+  build_time = "Sun Jun 25 02:12:35 2017";
 }
 ;;
 let set_version () =
