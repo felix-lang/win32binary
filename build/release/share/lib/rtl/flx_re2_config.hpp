84d1e11845c432d344fea3b7e60fa1fbdd30455a
skaller <skaller@users.sourceforge.net>
2017-09-09 20:47:15 +0000

    Appveyor rebuild

--- build/release/share/lib/rtl/flx_re2_config.hpp
+++ build/release/share/lib/rtl/flx_re2_config.hpp
@@ -1,4 +1,4 @@
-#line 1394 "C:/projects/felix/src/packages/strings.fdoc"
+#line 601 "C:/projects/felix/src/packages/regex.fdoc"
 #ifndef __FLX_RE2_CONFIG_H__
 #define __FLX_RE2_CONFIG_H__
 #include "flx_rtl_config.hpp"
